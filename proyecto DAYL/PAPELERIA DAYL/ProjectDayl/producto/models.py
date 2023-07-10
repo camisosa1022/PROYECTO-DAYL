dc3081ef4793eadbd5aff0bc2c108c4b9100ff39
Anfifix07 <jalargo60@misena.edu.co>
2023-07-09 22:01:53 -0500

    Actualización proyecto Dayls 2.0
    :3 que alegria, versión anterior al modulo de facturación y pedidos

--- proyecto DAYL/PAPELERIA DAYL/ProjectDayl/producto/models.py
+++ proyecto DAYL/PAPELERIA DAYL/ProjectDayl/producto/models.py
@@ -1,6 +1,17 @@
+from django.conf import settings
 from django.db import models
+from uuid import uuid4
+import os
+from django.db.models.signals import post_save, pre_save, pre_delete
+from django.dispatch import receiver
+
 def default_product_image():
     return 'imagen/productos/404.png'
+def nombreImagen(instance, filename):
+    extension = os.path.splitext(filename)[1]
+    new_filename = f"{uuid4().hex}{extension}"
+    return os.path.join('imagen/productos', new_filename)
+    
 # Create your models here.
 class Proveedor(models.Model):
     ciudad = models.CharField(max_length=30,help_text="Ciudad del proveedor")
@@ -15,7 +26,6 @@ class Proveedor(models.Model):
 class Producto(models.Model):
     proveedor_id = models.ForeignKey('Proveedor',on_delete=models.SET_NULL,null=True)
     nombre = models.CharField(max_length=30,help_text="Nombre del producto")
-    imagen = models.ImageField(upload_to='imagen/productos',null=True,blank=True,help_text="Imagen del producto", default=default_product_image)
     porcentaje_iva = models.DecimalField(max_digits=3,decimal_places=1,help_text="Porcentaje del iva ej = 16 - 19 ...")
     precio = models.DecimalField(max_digits=10,decimal_places=2,help_text="Valor del producto antes de iva")
     descripcion = models.TextField(help_text="Descripcion del producto")
@@ -32,6 +42,37 @@ class Subcategoria(models.Model):
         return f'{self.nombre} {self.categoria}'
 class Categoria(models.Model):
     nombre = models.CharField(max_length=30,help_text="Nombre de la categoria Principal")
+    imagen = models.ImageField(upload_to=nombreImagen,null=True,blank=True,help_text="Imagen de la categoria",default=default_product_image)
     
     def __str__(self) -> str:
         return f'{self.nombre}'
+class Color(models.Model):
+     imagen = models.ImageField(upload_to=nombreImagen,null=True,blank=True,help_text="Imagen del producto", default=default_product_image)
+     color = models.CharField(max_length=26)
+     stock = models.IntegerField(null=True, blank=True, help_text="Cantidad del producto del color agg")
+     producto_id = models.ForeignKey('Producto',on_delete=models.SET_NULL,null=True)
+     
+     def __str__(self) -> str:
+         return f'{self.producto_id.nombre} - {self.color} {self.id}'
+     
+@receiver(pre_save, sender=Color)
+def update_producto_stock(sender, instance, **kwargs):
+    if instance.pk:
+        # La imagen ya existe, calcular la diferencia de stock para actualizar el producto
+        old_instance = sender.objects.get(pk=instance.pk)
+        stock_diff = instance.stock - old_instance.stock
+    else:
+        # La imagen es nueva, agregar el stock al producto
+        stock_diff = instance.stock
+    
+    producto = instance.producto_id
+    producto.stock += stock_diff
+    producto.save()
+
+@receiver(pre_delete, sender=Categoria)
+@receiver(pre_delete, sender=Color)
+def delete_categoria_image(sender, instance, **kwargs):
+    if instance.imagen:
+        image_path = os.path.join(settings.MEDIA_ROOT, instance.imagen.name)
+        if os.path.isfile(image_path):
+            os.remove(image_path)
--- proyecto DAYL/PAPELERIA DAYL/ProjectDayl/producto/templatetags/custom_filters.py
+++ proyecto DAYL/PAPELERIA DAYL/ProjectDayl/producto/templatetags/custom_filters.py
@@ -5,3 +5,26 @@ register = template.Library()
 @register.simple_tag
 def increment(value):
     return value+1
+
+@register.simple_tag
+def calcular_precio_oferta_iva(valor,iva,oferta):
+
+    if oferta is None:
+        return valor*((iva/100)+1)
+    else:
+        return round((valor*(1-(oferta/100)))*((iva/100)+1),0)
+@register.simple_tag
+def calcular_precio_iva(producto):
+    return round(producto.precio*((producto.porcentaje_iva/100)+1),0)
+
+@register.simple_tag(takes_context=True)
+def primera_imagen(context,producto):
+    primera_imagen = producto.color_set.first()
+    if primera_imagen:
+        return primera_imagen.imagen.url
+    else:
+        return "unknown"
+@register.simple_tag(takes_context=True)
+def imagenes_color(context,producto):
+    imagenes = producto.color_set.all()
+    return imagenes
