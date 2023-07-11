2878b10b0e87a10f6e4844501ec2d2dc737dad07
camila sosa <110619532+camisosa1022@users.noreply.github.com>
2023-07-10 20:34:55 -0500

    dayl
    mejoras del prototipo

--- proyecto DAYL/PAPELERIA DAYL/HTML Alejandro producto/pagina dayl/js/index.js
+++ proyecto DAYL/PAPELERIA DAYL/HTML Alejandro producto/pagina dayl/js/index.js
@@ -14,21 +14,84 @@ $(document).ready(function () {
   $('.ui.dropdown').dropdown();
 
   $('.hamburger-menu').click(function () {
-      $(this).toggleClass('active');
-      $('.menu-hamburger').toggle();
+    $(this).toggleClass('active');
+    $('.menu-hamburger').toggle();
   });
 
   $(window).resize(function () {
-      var windowWidth = $(window).width();
-      if (windowWidth > 767) {
-          $('.hamburger-menu').removeClass('active');
-          $('.menu-hamburger').hide();
-      }
+    var windowWidth = $(window).width();
+    if (windowWidth > 767) {
+      $('.hamburger-menu').removeClass('active');
+      $('.menu-hamburger').hide();
+    }
   });
 });
-$(document).ready(function() {
+$(document).ready(function () {
   // Maneja el clic en el elemento principal del menú
-  $('.dropdown-main').on('click', function() {
+  $('.dropdown-main').on('click', function () {
     $(this).children('.menu-dropdown').toggleClass('active');
   });
 });
+
+//agregado 9 de julio//
+
+const menuContainer = document.querySelector('.menu-container');
+const contUl = document.querySelector('.cont-ul');
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+const hamburgerUl = document.querySelector('.hamburger-ul');
+
+function checkWindowSize() {
+  if (window.innerWidth < 1100) {
+    contUl.style.display = 'none';
+    hamburgerMenu.style.display = 'block';
+  } else {
+    contUl.style.display = 'flex';
+    hamburgerMenu.style.display = 'none';
+    hamburgerUl.style.display = 'none';
+  }
+}
+
+function toggleHamburgerMenu() {
+  if (hamburgerUl.style.display === 'block') {
+    hamburgerUl.style.display = 'none';
+  } else {
+    hamburgerUl.style.display = 'block';
+  }
+}
+
+checkWindowSize();
+
+window.addEventListener('resize', checkWindowSize);
+hamburgerMenu.addEventListener('click', toggleHamburgerMenu);
+
+
+//10 de julio//
+
+$(document).ready(function () {
+  $('.hamburger-icon').click(function () {
+    $('.hamburger-ul').slideToggle();
+  });
+
+  // Controla la apertura y desplazamiento del submenú
+  $('.hamburger-ul .develop').click(function () {
+    $(this).toggleClass('active');
+    $(this).find('.ul-second').slideToggle();
+    $(this).siblings().removeClass('active');
+    $(this).siblings().find('.ul-second').slideUp();
+    $('html, body').animate({
+      scrollTop: $(this).offset().top
+    }, 500);
+  });
+});
+
+function toggleHamburgerMenu() {
+  const hamburgerUl = document.querySelector('.hamburger-ul');
+  if (hamburgerUl.style.display === 'block') {
+    hamburgerUl.style.display = 'none';
+  } else {
+    hamburgerUl.style.display = 'block';
+  }
+}
+
+document.querySelector('.hamburger-icon').addEventListener('click', toggleHamburgerMenu);
+
--- proyecto DAYL/PAPELERIA DAYL/HTML Alejandro producto/pagina dayl/js/productos.js
+++ proyecto DAYL/PAPELERIA DAYL/HTML Alejandro producto/pagina dayl/js/productos.js
@@ -1,12 +1,64 @@
-$(document).ready(function() {
+$(document).ready(function () {
     // Aplicar truncado de texto a los elementos h3 y p con clase "clamp-2"
-    $('.clamp-2').each(function() {
+    $('.clamp-2').each(function () {
         var maxHeight = parseFloat($(this).css('line-height')) * 2; // Calcular altura máxima en píxeles (2 líneas)
         var textHeight = $(this).height(); // Obtener altura actual del contenido
         if (textHeight > maxHeight) {
-            $(this).text(function(index, currentText) {
+            $(this).text(function (index, currentText) {
                 return currentText.replace(/\W*\s(\S)*$/, '...'); // Truncar texto agregando puntos suspensivos
             });
         }
     });
+});
+
+//para nombre producto subcategoria agregado el 9/julio//
+
+// Función para actualizar los enlaces del breadcrumb
+function updateBreadcrumb(pageName) {
+    var breadcrumb = document.getElementById('breadcrumb');
+    breadcrumb.innerHTML = '<a href="#">Index</a> <i class="right angle icon"></i> <a href="#">' + pageName + '</a>';
+}
+
+// Simulación de cambio de página
+updateBreadcrumb('Categoría'); // Cambia el nombre de la categoría
+updateBreadcrumb('Subcategoría'); // Cambia el nombre de la subcategoría
+
+//filtros//
+$(document).ready(function () { //me guie de chat gpt para agregar la funcion de que se vieran los productos que se subian desde django//
+    $('#filtro').on('change', function () {
+        var selectedOption = $(this).val();
+
+        // Realizar una solicitud AJAX al servidor para obtener los productos filtrados
+        $.ajax({
+            url: '/obtener_productos/', // URL a la vista de Django que devuelve los productos en formato JSON
+            method: 'GET',
+            data: {
+                opcion: selectedOption
+            },
+            success: function (response) {
+                mostrarProductos(response.productos);
+            },
+            error: function (xhr, status, error) {
+                console.error('Error al obtener los productos:', error);
+            }
+        });
+    });
+
+    function mostrarProductos(productos) {
+        var $productosContainer = $('#productos');
+        $productosContainer.empty();
+
+        // Mostrar los productos en el contenedor
+        productos.forEach(function (producto) {
+            var $item = $('<div class="item">');
+            var $content = $('<div class="content">');
+            var $header = $('<div class="header">').text(producto.nombre);
+            var $description = $('<div class="description">').text(producto.descripcion);
+
+            $content.append($header);
+            $content.append($description);
+            $item.append($content);
+            $productosContainer.append($item);
+        });
+    }
 });
